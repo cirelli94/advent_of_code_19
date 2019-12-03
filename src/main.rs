99d97f8df58275893421b9ba86082e14db614b1c
Fabrizio Cirelli <cirelli94@gmail.com>
2019-12-03 15:53:58 +0100

    Added problem 1 part 2

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 mod prob01;
+mod prob01part2;
 mod utils;
 
 fn main() {
     println!("Hello, world!");
-    println!("{}", prob01::resolve_problem());
+    println!("prob01 part1: {}", prob01::resolve_problem());
+    println!("prob01 part2: {}", prob01part2::resolve_problem());
 }
---
+++ src/prob01part2.rs
@@ -0,0 +1,36 @@
+use super::utils;
+
+fn calc_fuel(mass: u32) -> u32 {
+    let mut fuel = 0;
+
+    if (mass as i32 / 3 - 2) > 0 {
+        fuel = mass / 3 - 2;
+        fuel += calc_fuel(fuel)
+    }
+
+    fuel
+}
+
+fn total_fuel(modules: &[u32]) -> u32 {
+    modules
+        .iter()
+        .map(|module| calc_fuel(*module))
+        .fold(0u32, |acc, fuel| acc + fuel)
+}
+
+fn load_modules(filename: &str) -> std::io::Result<Vec<u32>> {
+    let numbers: Vec<u32> = utils::split_whitespace_file(filename)
+        .iter()
+        .map(|x| x.parse::<u32>().unwrap())
+        .collect();
+
+    Ok(numbers)
+}
+
+pub fn resolve_problem() -> u32 {
+    let modules = load_modules("./data/01.txt").unwrap();
+
+    let fuel = total_fuel(&modules);
+
+    fuel
+}
