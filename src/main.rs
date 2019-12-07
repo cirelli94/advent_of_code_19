63f245406cd031a9420a5d12b9cc72464e4d7941
Fabrizio Cirelli <cirelli94@gmail.com>
2019-12-07 11:02:26 +0100

    Refined p02p2

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,9 @@ mod probs;
 pub mod utils;
 
 fn main() {
-    //println!("Hello, world!");
-    //println!("prob01 part1: {}", prob01::resolve());
-    //println!("prob01 part2: {}", prob01part2::resolve());
-    //println!("prob02 part1: {}", probs::p02::resolve());
-
-    let (noun, verb) = probs::p02p2::resolve();
-    println!("prob02 part2: {:?}", 100 * noun + verb);
+    println!("Hello, world!");
+    println!("prob01 part1: {}", probs::p01::resolve());
+    println!("prob01 part2: {}", probs::p01p2::resolve());
+    println!("prob02 part1: {}", probs::p02::resolve());
+    println!("prob02 part2: {:?}", probs::p02p2::resolve());
 }
--- src/probs/p02p2.rs
+++ src/probs/p02p2.rs
@@ -2,7 +2,7 @@ use super::intcode_computer as ic;
 use crate::utils;
 
 const EXPECTED_OUTPUT: usize = 19_690_720;
-pub fn resolve() -> (usize, usize) {
+pub fn resolve() -> usize {
     let vec = utils::split_char_file("./data/02.txt", ',');
     let memory = utils::convert_strings_to_usize(vec);
 
@@ -16,29 +16,11 @@ pub fn resolve() -> (usize, usize) {
 
             if let Some(result) = result {
                 if result == EXPECTED_OUTPUT {
-                    return (i, j);
+                    return 100 * i + j;
                 }
             }
         }
     }
 
     panic!();
-}
-
-#[test]
-fn test() {
-    //let map: Vec<(Vec<usize>, &[usize])> = vec![
-    //    (vec![1, 0, 0, 0, 99], &[2, 0, 0, 0, 99]),
-    //    (vec![2, 3, 0, 3, 99], &[2, 3, 0, 6, 99]),
-    //    (vec![2, 4, 4, 5, 99, 0], &[2, 4, 4, 5, 99, 9801]),
-    //    (
-    //        vec![1, 1, 1, 4, 99, 5, 6, 0, 99],
-    //        &[30, 1, 1, 4, 2, 5, 6, 0, 99],
-    //    ),
-    //];
-
-    //for (mut x, y) in map {
-    //    execute_program(&mut x);
-    //    assert_eq!(x, y);
-    //}
 }
